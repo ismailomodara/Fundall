fc66a1f07a6f6c26a891c8ee9264b651793fd790
omodara145 <omodara145@gmail.com>
2019-04-22 11:52:50 +0100

    Bug fixes, Thank-you page

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,23 @@
-$(document).ready(function() {
-  
-			$(document).ready(function(){
-			  // Add smooth scrolling to all cta
-			  $("a.nav-link").on('click', function(event) {
-			  	event.preventDefault();
-			    if (this.hash !== "") {
-			      // Prevent default anchor click behavior
-			      
-
-			      // Store hash
-			      var hash = this.hash;
-			      var offset = -100;
-			      // Using jQuery's animate() method to add smooth page scroll
-			      $('html, body').animate({
-			        scrollTop: $(hash).offset().top + offset
-			      }, 1500, function(){
-			   
-			        window.location.hash = hash;
-			      });
-			    }
-			 });
-		});
-
+$(document).ready(function(){
+	// Add smooth scrolling to all cta
+	$("a").on('click', function(e) {
+		
+		e.preventDefault();
+		if (this.hash !== "") {
+			// Prevent default anchor click behavior
+		
+			// Store hash
+			var hash = this.hash;
+			var offset = -100;
+			// Using jQuery's animate() method to add smooth page scroll
+			$('html, body').animate({
+				scrollTop: $(hash).offset().top + offset
+			}, 1200, function(){
+	 
+				window.location.hash = hash;
+			});
+		}
+		
+ });
+ return false
 });
